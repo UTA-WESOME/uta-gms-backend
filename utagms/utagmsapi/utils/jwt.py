d20dadc0c8a4972dd6e0706f79ec377f74f8a753
anras5 <anras1filip@gmail.com>
2023-08-06 13:57:19 +0200

    Add utils

---
+++ utagms/utagmsapi/utils/jwt.py
@@ -0,0 +1,23 @@
+import jwt
+from django.conf import settings
+
+from utagmsapi.models import User
+
+
+def get_user_from_jwt(token: str) -> User:
+    """Returns a User identified by id stored in JWT"""
+
+    # sanity check
+    if not token:
+        return None
+
+    # check to see if there is a word Bearer
+    if token.split()[0] != "Bearer":
+        return None
+
+    # get jwt token
+    token = token.split()[1]
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+
+    # retrieve User by id
+    return User.objects.filter(id=payload['id']).first()
