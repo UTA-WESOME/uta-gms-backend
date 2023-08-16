7bb9022e76e9ac75132ff6fb32d7df08073d69e9
anras5 <anras1filip@gmail.com>
2023-08-16 22:45:24 +0200

    Clean up serializers

--- utagms/utagmsapi/serializers.py
+++ utagms/utagmsapi/serializers.py
@@ -5,11 +5,6 @@ from utagmsapi import models
 
 
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(max_length=32, help_text="User email")
-    password = serializers.CharField(max_length=255, help_text="User password")
-    name = serializers.CharField(max_length=64, help_text="User first name")
-    surname = serializers.CharField(max_length=64, help_text="User last name")
-
     class Meta:
         model = models.User
         fields = "__all__"
@@ -27,10 +22,6 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=64, help_text="Project name")
-    description = serializers.CharField(max_length=256, help_text="Project description")
-    shareable = serializers.BooleanField(help_text="Project shareability")
-
     class Meta:
         model = models.Project
         exclude = ['user']
@@ -38,36 +29,24 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 
 class CriterionSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=64, help_text="Criterion name")
-    gain = serializers.BooleanField(help_text="Criterion is the type of gain")
-
     class Meta:
         model = models.Criterion
         fields = "__all__"
 
 
 class AlternativeSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=64, help_text="Alternative name")
-    reference_ranking = serializers.IntegerField(help_text="Alternative reference ranking")
-    ranking = serializers.IntegerField(help_text="Alternative ranking")
-
     class Meta:
         model = models.Alternative
         fields = "__all__"
 
 
 class PerformanceSerializer(serializers.ModelSerializer):
-    value = serializers.FloatField(help_text="Performance value")
-
     class Meta:
         model = models.Performance
         fields = "__all__"
 
 
 class CriterionFunctionSerializer(serializers.ModelSerializer):
-    ordinate = serializers.FloatField(help_text="Ordinate")
-    abscissa = serializers.FloatField(help_text="Abscissa")
-
     class Meta:
         model = models.CriterionFunction
         fields = "__all__"
